2b4dd2e50db63f9088e1192fdf18c5097a118eff
rifdoalam <rifdoalam@gmail.com>
2023-06-12 22:21:25 +0700

    update mobile

---
+++ app/src/main/java/com/example/myapplication/Wisata.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Wisata : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.wisatalist)
+    }
+}
